5b5923bd1c519a02e3c0f2704d70fdefb64ceaf4
Sämann <nicole.saemann@cgi.com>
2018-08-08 09:41:54 +0200

    Initial commit

--- start banking.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\nicole.saemann\Documents\02_Bachelor Arbeit\31 PWA\banking-app
-ng serve --o
--- start hero.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\nicole.saemann\Documents\PWA template\PWA tour of heroes\angular-tour-of-heroes
-ng serve --o --port 4300
-
